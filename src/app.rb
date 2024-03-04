f1527f0a5a7523c1f70e514275d68225caf8eeef
erik-gustafsson-nti-johanneberg <42681756+MrErik7@users.noreply.github.com>
2024-03-04 22:33:08 +0100

    1.5
    - fixade rating systemet
    - löste en sökfunktion
    - patchade lite buggar med det hela
    - la till artists som ett eget table i databasen och gjorde om koden därefter
    - La även till bilder för artister
    - Samt att en användare nu måste lägga till en artist först innan personen lägger till en release

--- src/app.rb
+++ src/app.rb
@@ -12,57 +12,84 @@ class App < Sinatra::Base
     get '/' do
         session[:user_id] = 1 
         @releases = db.execute("SELECT * FROM releases")
+        @artists = db.execute("SELECT * FROM artists")
+
         erb :index
        # puts("test")
     end
 
+    # -- GENERAL FUNCTIONS ---
+    def get_artist_name_from_id(artist_id)
+        puts(artist_id.to_s)
+        artist = db.execute("SELECT name FROM artists WHERE id = ?", artist_id).first
+        artist ? artist["name"] : "Unknown"
+    end
+
+    #---
+
+    # -- RELEASES --
     # --- ADD A RELEASE ---
-    get '/add' do
-        erb :add
+    get '/release/add' do
+        @artists = db.execute("SELECT * FROM artists")
+
+        erb :add_release
     end
 
     post '/release/add' do
         id = params["id"]
-        name = params["name"]
-        artist = params["artist"]
+        title = params["title"]
+        artist_id = params["artist_id"]
         length = params["length"]
         type = params["type"]
         genre = params["genre"]
         release_date = params["release_date"]
         artwork_file = params["release_artwork"]
-        
-        # Save the uploaded file to the server
-        File.open('public/artwork/' + artwork_file[:filename], "w") do |f|
-            f.write(artwork_file[:tempfile].read)
+
+        # Check if there is a file uploaded
+        if !(artwork_file == nil)
+            # Save the uploaded file to the server
+            File.open('public/artwork/' + artwork_file[:filename], "w") do |f|
+                f.write(artwork_file[:tempfile].read)
+            end
+            image_path = "/artwork/" + artwork_file[:filename]
+        else
+            image_path = nil
         end
 
-        puts(artwork_file[:filename])
 
-        query = 'INSERT INTO releases (name, artist, length, type, genre, release_date, image_path) VALUES (?, ?, ?, ?, ?, ?, ?) RETURNING id'
-        result = db.execute(query, name, artist, length, type, genre, release_date, "/artwork/" + artwork_file[:filename]).first 
 
+        # Insert the release data into the database
+        query = 'INSERT INTO releases (title, artist_id, length, type, genre, release_date, image_path) VALUES (?, ?, ?, ?, ?, ?, ?) RETURNING id'
+        result = db.execute(query, title, artist_id, length, type, genre, release_date, image_path).first 
         redirect "/"
     end
     # ------
-
-
     # --- REMOVE A RELEASE ---
     post '/release/remove/:id' do |id| 
+
+        # Remove reviews associated with the release
+        db.execute('DELETE FROM reviews WHERE release_id = ?', id)
+
+        # Remove the release
         db.execute('DELETE FROM releases WHERE id = ?', id)
+        
         redirect "/"
     end
-    # ------
 
+    # ------
     # --- EDIT A RELEASE ---
-    get '/edit/:id' do |id|
+    get '/release/edit/:id' do |id|
         @release_info = db.execute("SELECT * FROM releases WHERE id = ?", id)
         @release_info = @release_info[0]
-        erb :edit
+
+        @artists = db.execute("SELECT * FROM artists")
+
+        erb :edit_release
     end
 
     post '/release/edit/:id' do |id|
-        name = params["name"]
-        artist = params["artist"]
+        title = params["title"]
+        artist_id = params["artist_id"]
         length = params["length"]
         type = params["type"]
         genre = params["genre"]
@@ -71,8 +98,8 @@ class App < Sinatra::Base
 
         # First check if artwork file is empty since its not neccesarry for the user to update, if it is --> then just update the rest of the information
         if artwork_file == nil
-            query = "UPDATE releases SET name = ?, artist = ?, length = ?, rating = ?, type = ?, genre = ?, release_date = ? WHERE id = ?"
-            result = db.execute(query, name, artist, length, rating, type, genre, release_date, id)
+            query = "UPDATE releases SET title = ?, artist_id = ?, length = ?, type = ?, genre = ?, release_date = ? WHERE id = ?"
+            result = db.execute(query, title, artist_id, length, type, genre, release_date, id)
 
         else 
 
@@ -84,21 +111,32 @@ class App < Sinatra::Base
             end
 
             # Update the database        
-            query = "UPDATE releases SET name = ?, artist = ?, length = ?, rating = ?, type = ?, genre = ?, release_date = ?, image_path = ? WHERE id = ?"
-            result = db.execute(query, name, artist, length, rating, type, genre, release_date, "/artwork/" + artwork_file[:filename], id)
+            query = "UPDATE releases SET title = ?, artist_id = ?, length = ?, rating = ?, type = ?, genre = ?, release_date = ?, image_path = ? WHERE id = ?"
+            result = db.execute(query, title, artist_id, length, rating, type, genre, release_date, "/artwork/" + artwork_file[:filename], id)
 
          end
             
         redirect "/"
     end
     # ------
-
     # --- VIEW A RELEASE ---
     get '/view/:id' do |id|
         @release_info = db.execute("SELECT * FROM releases WHERE id = ?", id)
         @release_info = @release_info[0]
 
         @review_info = db.execute("SELECT * FROM reviews WHERE release_id = ?", id)
+       
+        # Calculate the total rating of the release
+        total_rating = 0
+        count = 0
+        unless @review_info.empty?
+            @review_info.each do |review|
+                total_rating += review['review_rating']
+                count += 1
+            end
+        end
+    
+        @total_rating = count == 0 ? "None" : (total_rating / count).round(2)    
 
         erb :view
     end
@@ -112,7 +150,110 @@ class App < Sinatra::Base
         query = 'INSERT INTO reviews (release_id, username, review_rating, review_text) VALUES (?, ?, ?, ?) RETURNING id'
         result = db.execute(query, release_id, username, review_rating, review_text)
     
+        redirect "/view/#{release_id}"
+    end
+
+    # -- ARTIST --
+    # --- ADD AN ARTIST ---
+    get '/artist/add' do
+        erb :add_artist
+    end
+
+    post '/artist/add' do
+        id = params["id"]
+        name = params["name"]
+        bio = params["bio"]
+        country = params["country"]
+        city = params["city"]
+        logo_file = params["logo"]
+
+        # Check if there is a file uploaded
+        if !(logo_file == nil)
+            # Save the uploaded file to the server
+            File.open('public/artwork/' + logo_file[:filename], "w") do |f|
+                f.write(logo_file[:tempfile].read)
+            end
+            image_path = "/artwork/" + logo_file[:filename]
+         else
+        image_path = nil
+    end
+
+        query = 'INSERT INTO artists (name, bio, country, city, image_path) VALUES (?, ?, ?, ?, ?) RETURNING id'
+        result = db.execute(query, name, bio, country, city, image_path)
+
         redirect "/"
+    end
+    # ------
+    # --- REMOVE AN ARTIST ---
+    post '/artist/remove/:id' do |id| 
+        # Remove releases associated with the artist
+        db.execute('DELETE FROM releases WHERE artist_id = ?', id)
+        
+        # Remove reviews associated with the releases deleted
+        db.execute('DELETE FROM reviews WHERE release_id NOT IN (SELECT id FROM releases)')
+    
+        # Remove the artist
+        db.execute('DELETE FROM artists WHERE id = ?', id)
+        
+        redirect "/"
+    end
+        
+
+     # --- EDIT AN ARTIST ---
+     get '/artist/edit/:id' do |id|
+        @artist_info = db.execute("SELECT * FROM artists WHERE id = ?", id)
+        @artist_info = @artist_info[0]
+        erb :edit_artist
+    end
+
+    post '/artist/edit/:id' do |id|
+        id = params["id"]
+        name = params["name"]
+        bio = params["bio"]
+        country = params["country"]
+        city = params["city"]
+        logo_file = params["logo"]
+
+        # First check if artwork file is empty since its not neccesarry for the user to update, if it is --> then just update the rest of the information
+        if logo_file == nil
+            query = "UPDATE artists SET name = ?, bio = ?, country = ?, city = ? WHERE id = ?"
+            result = db.execute(query, name, bio, country, city, id)
+
+        else 
+
+            # [to-do] Remove the old image
+
+            # Save the uploaded file to the server
+            image_path = "/logos/" + logo_file[:filename]
+
+            File.open('public/' + image_path, "w") do |f|
+                f.write(logo_file[:tempfile].read)
+            end
+
+            # Update the database        
+            query = "UPDATE artists SET name = ?, bio = ?, country = ?, city = ?, image_path = ? WHERE id = ?"
+            result = db.execute(query, name, bio, country, city, image_path, id)
+
+         end
+            
+        redirect "/"
+    end
+    # ------
+   
+
+    # --- SEARCH FOR A RELEASE OR ARTIST --- (As of right now one can only search for releases, this may be uodated in the future so also artists can be searched)
+    get "/release/search/" do
+        # Get the search query from params
+        @query = params[:query]
+
+        # Perform the search based on the query 
+        @release_results = db.execute("SELECT * FROM releases WHERE title LIKE ?", "%#{@query}%")
+        @artist_results = db.execute("SELECT * FROM artists WHERE name LIKE ?", "%#{@query}%")
+
+        # Render the search results template
+        erb :search_result
+
+
     end
 
 
